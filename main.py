d12c354769dcb9b03e0654eaf276590c037fc079
AkashNagaraj <akash.nagaraj1@gmail.com>
2021-06-07 08:17:58 -0400

    adding self attn

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 from sub_dataset import *
 from vector import *
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-import math
-
-
-class Skip_Gram(nn.Module):
-    def __init__(self, vocab_size, embedding_size, cont_size):
-        super(Skip_Gram,self).__init__()
-        self.context_size = cont_size
-        self.embeddings = nn.Embedding(vocab_size, embedding_size)
-        self.linear1 = nn.Linear(cont_size*embedding_size, 128)
-        self.linear2 = nn.Linear(128, vocab_size)
-
-    def forward_embedding(self,inputs):
-        embeds = self.embeddings(inputs).view((1,-1)) # inputs:[1,2,3,4] or [[1,2,3,4]]
-        out = F.relu(self.linear1(embeds))
-        out = F.sigmoid(self.linear2(out))
-        log_probs = F.log_softmax(out,dim=1)
-        return log_probs
-        
-    def combine_embedding(self,t_inp,c_inp):
-        t_embeds = self.embeddings(t_inp)
-        c_embeds = self.embeddings(c_inp)
-        comb = t_embeds*c_embeds.view(self.context_size,-1)
-        return comb
+from model import *
 
 
 def train_model(data, vocab_size, embedding_size, context_size, device):
     losses = []
     loss_function = nn.NLLLoss()
-    
+
     model = Skip_Gram(len(vocab_size), embedding_size, context_size).to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.0001)
 
@@ -43,15 +15,25 @@ def train_model(data, vocab_size, embedding_size, context_size, device):
         for mini_batch in data:
             target = torch.tensor(mini_batch[0], dtype=torch.long).to(device)
             context = torch.tensor([mini_batch[1]], dtype=torch.long).to(device)
-            labels = torch.tensor([mini_batch[2]], dtype=torch.long).to(device) 
-            
+            labels = torch.tensor(mini_batch[2], dtype=torch.long).to(device)
+
             model.zero_grad()
-            # Learn Embedding 
-            target_loss = model.forward_embedding(target) 
-            #context_loss = model.forward_embedding(context)
-            # Combine target and context embedding to one
-            combine_embeddings = model.combine_embedding(target, context)
-            # Pass through CNN
+            # ==== Learn Embedding ==== # 
+            # target_loss = model.forward_embedding(target).to(device)
+            # context_loss = model.forward_embedding(context).to(device)
+            
+            # ==== Combine target and context embedding by Hamadard Product ==== #
+            combine_embeddings = model.combine_embedding(target, context).to(device)
+            # print(combine_embeddings.data.shape)
+            
+            # ==== Self attention of combine embeddings ==== #
+            self_attn = torch.matmul(combine_embeddings, torch.transpose(combine_embeddings, 0, 1)) 
+            #print(self_attn.data.shape)
+            #loss = loss_function(self_attn,labels)
+            #print(torch.exp(loss)) # Get probability
+            
+            # ==== CNN ==== #
+            
 
     """
             loss = loss_function(log_probs,torch.tensor([np.arange(28)],dtype=torch.long)) # labels
@@ -63,22 +45,9 @@ def train_model(data, vocab_size, embedding_size, context_size, device):
     """
 
 
-def build_batch(data, split_data):
-    ## Add random ##
-    t = [data[i][0][0] for i in range(0,len(data))]
-    c = [data[i][1][0] for i in range(0,len(data))]
-    l = [data[i][2][0] for i in range(0,len(data))]
-   
-    targets = [t[i:i+split_data] if i+split_data<=len(data) else t[i:] for i in range(0,len(data),split_data)]
-    context = [c[i:i+split_data] if i+split_data<=len(data) else t[i:] for i in range(0,len(data),split_data)]
-    labels = [l[i:i+split_data] if i+split_data<=len(data) else t[i:] for i in range(0,len(data),split_data)]
-    
-    return (tuple(zip(targets,context,labels)))
-
-
 def main():
     lines , freq_count, dict_ = choose_sentences() # Choose 1000 sentences with most chars and highest char frequency 
-    data_vector, char_dict, window = build_char_data(lines, dict_, test=False) # 10% masked 
+    data_vector, char_dict, window = build_char_data(lines, dict_, test=True)  
 
     batch_size = 10
     remainder = len(data_vector)%batch_size
@@ -90,7 +59,6 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     train_model(data, char_dict, embed_size, batch_size, device)
     """
-    #build_word_embedding() 
     new_embedding_ch = self_attention(embedding_ch)
     convolution(new_embedding_ch)
     """
--- model.py
+++ model.py
@@ -1,42 +1,42 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
-import numpy as np
+import torch.nn.functional as F
+import torch.optim as optim
+
+import math
 
 
-# Model to accept (1,10) input and output (1,10)
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.embedding = nn.Embedding(10,100)
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(10, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-            nn.ReLU()
-        )
+class Skip_Gram(nn.Module):
+    def __init__(self, vocab_size, embedding_size, cont_size):
+        super(Skip_Gram,self).__init__()
+        self.context_size = cont_size
+        self.embeddings = nn.Embedding(vocab_size, embedding_size)
+        self.linear1 = nn.Linear(cont_size*embedding_size, 128)
+        self.linear2 = nn.Linear(128, vocab_size)
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+    def forward_embedding(self,inputs):
+        embeds = self.embeddings(inputs).view((1,-1)) # inputs:[1,2,3,4] or [[1,2,3,4]]
+        out = F.relu(self.linear1(embeds))
+        out = F.sigmoid(self.linear2(out))
+        log_probs = F.log_softmax(out,dim=1)
+        return log_probs
+        
+    def combine_embedding(self,t_inp,c_inp):
+        t_embeds = self.embeddings(t_inp)
+        c_embeds = self.embeddings(c_inp)
+        comb = t_embeds*c_embeds.view(self.context_size,-1)
+        return comb
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = NeuralNetwork().to(device)
-
-input_ = torch.randn(10,10)
-logit = model(input_)
-out = nn.Softmax(dim=1)(logit)
-target = torch.empty(10,dtype=torch.long)
-
-#print(input_.data.shape, target.data.shape)
-print(input_)
-print(target)
-loss = nn.CrossEntropyLoss()#Crossentropy 
-loss = torch.sqrt(loss(out,target))
-
+def build_batch(data, split_data):
+    ## Add random ##
+    t = [data[i][0][0] for i in range(0,len(data))]
+    c = [data[i][1][0] for i in range(0,len(data))]
+    l = [data[i][2][0] for i in range(0,len(data))]
+   
+    targets = [t[i:i+split_data] if i+split_data<=len(data) else t[i:] for i in range(0,len(data),split_data)]
+    context = [c[i:i+split_data] if i+split_data<=len(data) else t[i:] for i in range(0,len(data),split_data)]
+    labels = [l[i:i+split_data] if i+split_data<=len(data) else t[i:] for i in range(0,len(data),split_data)]
+    
+    return (tuple(zip(targets,context,labels)))
 
